eb4050f888f1fc46c850a7b74abee649e08d6322
DanielArmR <daniel62armas@gmail.com>
2021-03-22 21:20:45 -0600

    Add methods for hash table

--- hashTable/hashtable.js
+++ hashTable/hashtable.js
@@ -10,7 +10,7 @@ class HashTable {
         }
         return hash;
     }
-    set(key, value) {
+    set(key, value = 2021) {
         const address = this.hashMethod(key);
         if(!this.data[address]){
             this.data[address] = [];
@@ -18,8 +18,51 @@ class HashTable {
         this.data[address].push([key, value]);
         return this.data;
     }
+    get(key) {
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if(currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if(currentBucket[i][0] === key){
+                    return currentBucket[i];
+                }
+            }
+        }
+        return `No existe`;
+    }
+    remove(key) {
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if(currentBucket){
+            for (let i = 0; i < currentBucket.length; i++) {
+                if(currentBucket[i][0] === key){
+                    const item = currentBucket[i];
+                    currentBucket.splice(i, 1);
+                    return item;
+                }
+            }
+        }
+        return `No existe`;
+    }
+    getAllKeys() {
+        const keys = [];
+        for (let i = 0; i < this.data.length; i++) {
+            if(this.data[i]){
+                for (let j = 0; j < this.data[i].length; j++) {
+                    keys.push(this.data[i][j][0]);
+                }
+            }
+        }
+        return keys;
+    }
 }
 const myHashTable = new HashTable(50);
 myHashTable.set('Diego', 1990);
-myHashTable.set('Daniel', 2000);
-myHashTable.set('Sandra', 2000);
+myHashTable.set('Daniel', 2002);
+myHashTable.set('Sandra', 1999);
+myHashTable.set('Mariana', 2003);
+myHashTable.get('Daniel');
+myHashTable.get('Dan');
+myHashTable.remove('Sandra');
+myHashTable.set('Dominic');
+myHashTable.getAllKeys();
