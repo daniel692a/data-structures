e3eed52ebae131f24c6c02982c4dc31693603bc2
DanielArmR <daniel62armas@gmail.com>
2021-03-31 21:25:47 -0600

    Building a graph

--- graphs/graphClass.js
+++ graphs/graphClass.js
@@ -3,5 +3,27 @@ class Graph {
         this.nodes = 0;
         this.adjacentList = {}
     }
+    addVertex(node) {
+        this.adjacentList[node] = [];
+        this.nodes++;
+    }
+    addEdge(node1, node2) {
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
+    }
 }
 const graph = new Graph();
+graph.addVertex(1);
+graph.addVertex(3);
+graph.addVertex(4);
+graph.addVertex(5);
+graph.addVertex(6);
+graph.addVertex(8);
+
+graph.addEdge(1, 3);
+graph.addEdge(1, 6);
+graph.addEdge(1, 4);
+graph.addEdge(4, 8);
+graph.addEdge(4, 5);
+graph.addEdge(5, 3);
+graph.addEdge(6, 3);
